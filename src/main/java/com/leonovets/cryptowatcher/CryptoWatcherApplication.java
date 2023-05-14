b736b1b85787995f0c442c654a2e4b2dac15cb60
Mikhail Leonovets <mikhail.leonovets@gmail.com>
2023-05-14 16:38:35 +0300

    spring structure created

---
+++ src/main/java/com/leonovets/cryptowatcher/CryptoWatcherApplication.java
@@ -0,0 +1,13 @@
+package com.leonovets.cryptowatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoWatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoWatcherApplication.class, args);
+	}
+
+}
